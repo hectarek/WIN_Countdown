a844ae7cadf081fdef4f20fc5477b8dd7c8595ce
Hector Gonzalez <hectarek13@gmail.com>
2020-05-20 17:26:03 -0400

    typography changes and date format

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 // JS for WIN Cohort Calculator 
+// Idea Cred goes out to Pernell Grant
 
 //Grabbing the appropriate dates
-var startDate = new Date("March 09, 2020").getTime();
-var graduationDate = new Date("September 04, 2020").getTime();
+let startDate = new Date("March 09, 2020").getTime();
+let graduationDate = new Date("August 21, 2020").getTime();
+
+//Arrays to represent the string version of the date
+let monthNames = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October","November","December"]
+let dayOfWeekNames = ["Sun", 'Mon', "Tues", "Wed", "Thurs", "Fri", "Sat"]
 
 //Total duration of the class
-var programDuration = graduationDate - startDate;
+let programDuration = graduationDate - startDate;
 
 //Function to constantly refresh date and progress
-var x = setInterval(() => {
+let x = setInterval(() => {
 
-    var currentDate = new Date();
+    let currentDate = new Date();
 
-    var timeLeft = graduationDate - currentDate.getTime();
+    //Breaking up date so I can format it better.
+    let dayOfWeek = currentDate.getDay();
+    let month = currentDate.getMonth();
+    let date = currentDate.getDate();
+    let year = currentDate.getFullYear();
 
-    var percentCompleted = 100 - ((timeLeft/programDuration).toFixed(2) * 100);
+    let dateFormat = dayOfWeekNames[dayOfWeek] + ", " + monthNames[month] + " " + date + ", " + year;
+
+    let timeLeft = graduationDate - currentDate.getTime();
+
+    let percentCompleted = 100 - ((timeLeft/programDuration).toFixed(2) * 100);
 
     console.log(percentCompleted)
 
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    // var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    // var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    // let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    var dateElement = document.getElementById("date")
-    var daysElement = document.getElementById("day-num")
-    var daysHourElement = document.getElementById("hour-num")
-    var progress = document.getElementById("progress")
+    let dateElement = document.getElementById("date")
+    let daysElement = document.getElementById("day-num")
+    let daysHourElement = document.getElementById("hour-num")
+    let progress = document.getElementById("progress")
 
-    dateElement.textContent = currentDate.toDateString();
+    dateElement.textContent = dateFormat;
 
     daysElement.textContent = days;
 
